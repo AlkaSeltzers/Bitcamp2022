2d21be221666fc2667574af1586616dbe82b536a
AlkaSeltzers <32341871+AlkaSeltzers@users.noreply.github.com>
2022-04-09 19:59:15 -0400

    Add about.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,10 @@
 var homepage = require('./homepage');
 var data_vis = require('./data_vis');
+var about = require('./about');
 
 module.exports.set = function(app){
     homepage.set(app);
     data_vis.set(app);
+    about.set(app);
 
 };
