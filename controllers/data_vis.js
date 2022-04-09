3ffd4a5755ae8901b686ef20e37e1b604d0ee470
AlkaSeltzers <32341871+AlkaSeltzers@users.noreply.github.com>
2022-04-09 19:34:20 -0400

    Add json contents to file

--- controllers/data_vis.js
+++ controllers/data_vis.js
@@ -1,3 +1,13 @@
+var fs = require('fs');
+
+
+exports.middleWare = function(req, res, next){
+    var body = fs.readFileSync("resources/data.json").toString('utf-8')
+    var obj = JSON.parse(body)
+    res.locals.obj = obj
+    next()
+}
+
 module.exports.set = function(app){
     app.get('/data_vis', function(req, res){
         res.render("dataVis");
